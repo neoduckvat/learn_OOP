1a3646b11c043fc9241e8a784333c98302e507a7
Neoduckvat <neoduckvat@gmail.com>
2025-09-25 00:46:41 +0300

    Add vacation days counter

--- vacation_check.py
+++ vacation_check.py
@@ -1,14 +1,18 @@
 class Employee:
-    # Вместо инструкции pass напишите свой коd
     vacation_days = 28
-    def __init__(self,first_name,second_name,gender):
+
+    def __init__(self, first_name, second_name, gender):
         self.first_name = first_name
         self.second_name = second_name
         self.gender = gender
-# Создайте экземпляры класса Employee с различными значениями атрибутов.
-employee1 = Employee("Роберт","Крузо","м")
-employee2 = Employee("Яша","Лава","ж")
+        self.remaining_vacation_days = Employee.vacation_days
+    
+    def consume_vacation(self,days):
+        self.remaining_vacation_days -= days
 
-# Допишите код для вывода информации о сотрудниках.
-print(f'Имя: {employee1.first_name}, Фамилия: {employee1.second_name}, Пол: {employee1.gender}, Отпускных дней в году: {employee1.vacation_days}.')
-print(f'Имя: {employee2.first_name}, Фамилия: {employee2.second_name}, Пол: {employee2.gender}, Отпускных дней в году: {employee2.vacation_days}.')
+    def get_vacation_details(self):
+        return f'Остаток отпускных дней: {self.remaining_vacation_days}.'
+    
+employee = Employee('Роберт','Крузо','м')
+employee.consume_vacation(7)
+print(employee.get_vacation_details())
