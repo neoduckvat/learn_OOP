17e97162d193d3053a155efdae0c0fba63415b39
Neoduckvat <neoduckvat@gmail.com>
2025-09-25 20:09:17 +0300

    Practical task on monitoring bacteria

---
+++ practice1/practice1_1.py
@@ -0,0 +1,29 @@
+class BacteriaProducer:
+    
+    def __init__(self, max_bacteria):
+        self.max_bacteria = max_bacteria
+        self.current_bacteria_count = 0 
+
+    def create(self):
+        if self.current_bacteria_count == self.max_bacteria:
+            print("Нет места под новую бактерию")
+        else:
+            self.current_bacteria_count += 1
+            print(f'Добавлена одна бактерия. Бактерий в колонии: {self.current_bacteria_count}')
+
+    def delete(self):
+        if self.current_bacteria_count == 0:
+            print('В популяции нет бактерий, удалять нечего')
+        else:
+            self.current_bacteria_count -= 1
+            print('Одна бактерия удалена. Бактерий в колонии: {self.current_bacteria_count}')
+
+
+# Пример запуска для самопроверки
+bacteria_producer = BacteriaProducer(max_bacteria=3)
+bacteria_producer.delete()
+bacteria_producer.create()
+bacteria_producer.create()
+bacteria_producer.create()
+bacteria_producer.create()
+bacteria_producer.delete()
